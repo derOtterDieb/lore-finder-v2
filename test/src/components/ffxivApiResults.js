77dcf2f206a608d54d99745b04f49663adc6bb57
derOtterDieb <50959375+derOtterDieb@users.noreply.github.com>
2019-10-18 08:56:02 +0200

    test

---
+++ test/src/components/ffxivApiResults.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FfxivApiResults = ({ ffxivApiResults }) => {
+    return (
+        <div className="Result-container">
+            {ffxivApiResults.Name_fr}
+        </div>
+    );
+}
+
+export default FfxivApiResults;
